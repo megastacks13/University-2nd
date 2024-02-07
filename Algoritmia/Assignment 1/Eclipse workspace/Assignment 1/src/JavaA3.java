6370a6806322a9dd10dfee06f6ac306f4994e84d
Jaime Alonso Fernández <mega13dev@gmail.com>
2024-02-07 15:49:39 +0100

    Assignment 1 missing conclusion (algorithmics)

--- Algoritmia/Assignment 1/Eclipse workspace/Assignment 1/src/JavaA3.java
+++ Algoritmia/Assignment 1/Eclipse workspace/Assignment 1/src/JavaA3.java
@@ -11,7 +11,7 @@ public class JavaA3 extends Java_basis {
 	
 	
 	public boolean isPrime(int n) {
-		for (int i = 2; i < Math.floorDiv(n, 2) + 1; i++) {
+		for (int i = 2; i < n/2 + 1; i++) {
 			if (n%i == 0) return false;
 		}
 		return true;
